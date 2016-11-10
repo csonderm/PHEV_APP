8c970605c5b9d8b049fc08a829ce6b60fd1a7a83
Aaron Striegel <striegel@Aaron-Striegels-MacBook-Pro.local>
2016-11-10 17:24:38 -0500

    started making view controller classes

---
+++ PHEV/VehicleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VehicleViewController.swift
+//  PHEV
+//
+//  Created by Aaron Striegel on 11/10/16.
+//  Copyright © 2016 Claire Sonderman. All rights reserved.
+//
+
+import UIKit
+
+class VehicleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
